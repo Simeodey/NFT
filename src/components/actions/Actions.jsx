4439e9616871f36ca7bbd37c09040273d1cd4228
Simeodey <adamand12@abv.bg>
2023-01-21 12:31:56 +0200

    wweqe

---
+++ src/components/actions/Actions.jsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import classNames from 'classnames';
+import styles from './Actions.module.scss';
+
+import { Select, Grid, MenuItem, Box, Container } from '@mui/material';
+import Card from '../card/Card';
+
+export default function Trending({ cards = [] }) {
+  return (
+    <div className={classNames(styles.wrapper)}>
+      <Container maxWidth="lg">
+        <Box display="flex" justifyContent="space-between">
+          <h1 className={classNames(styles.header)}>🔥 Live Actions</h1>
+          <Select
+            sx={{
+              width: '200px',
+              borderRadius: '10px',
+            }}
+          >
+            <MenuItem value={1}>This week</MenuItem>
+          </Select>
+        </Box>
+        <Grid container spacing={2}>
+          {cards.map((card, index) => (
+            <Grid item xs={3} key={card.name + index}>
+              <Card
+                name={card.name}
+                likes={1100}
+                mediaUrl={card.mediaUrl}
+                user={card.user}
+                price={card.price}
+                currency={card.currency}
+                timeLeft={card.timeLeft}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
+}
